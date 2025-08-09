c45759c3266cdc303143896a654d16b176726fff
PyAdminRU <zh.vitaliy92@gmail.com>
2025-08-09 22:19:37 +0300

    Получение данных с помощью requests с сайта https://tronscan.org/.

--- main.py
+++ main.py
@@ -9,12 +9,39 @@ from aiogram.types import Message
 
 from handler.handler import register_handler
 from keyboards.keyboards import register_keyboard
-from system.system import router, dp, bot
+from system.system import router, dp, bot, WALLET
+import requests
+
+# https://tronscan.org/
+
+# GET https://apilist.tronscanapi.com/api/account/wallet?address=<ваш адрес>&asset_type=0
+
+#GET https://apilist.tronscanapi.com/api/transfer/trx?address=<ваш адрес>&start=0&limit=20&direction=0
+
+#GET https://apilist.tronscanapi.com/api/account/analysis?address=<ваш адрес>&type=0&start_timestamp=<ms>&end_timestamp=<ms>
+
+
+def get_tron_balance(address: str):
+    response = requests.get(f"https://apilist.tronscanapi.com/api/account/wallet?address={address}&asset_type=0")
+    print(response.status_code)  # Выводит статус-код ответа
+    print(response.json())  # Выводит JSON-ответ
+
+    response = requests.get(f"https://apilist.tronscanapi.com/api/transfer/trx?address={address}&start=0&limit=20&direction=0")
+    print(response.status_code)  # Выводит статус-код ответа
+    print(response.json())  # Выводит JSON-ответ
+
+    response = requests.get(
+        f"https://apilist.tronscanapi.com/api/account/analysis?address={address}&type=0&start_timestamp=<ms>&end_timestamp=<ms>")
+    print(response.status_code)  # Выводит статус-код ответа
+    print(response.json())  # Выводит JSON-ответ
 
 
 # Хендлер команды /start
 @router.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
+
+    get_tron_balance(address=WALLET)
+
     await message.answer(
         f"Hello, {html.bold(message.from_user.full_name)}!",
         reply_markup=register_keyboard()
--- system/system.py
+++ system/system.py
@@ -5,6 +5,7 @@ from aiogram import Bot, Dispatcher, Router
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from dotenv import load_dotenv
+from loguru import logger
 
 # Загружаем переменные окружения из .env
 load_dotenv()
@@ -13,6 +14,12 @@ load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
 if not TOKEN:
     raise ValueError("❌ BOT_TOKEN не найден! Укажите его в .env или переменной окружения.")
+#  Получаем кошелек пользователя
+WALLET = os.getenv("WALLET")
+if not WALLET:
+    raise ValueError("❌ WALLET не найден! Укажите его в .env или переменной окружения.")
+
+logger.debug(f"TOKEN: {TOKEN}, WALLET: {WALLET}")
 
 # Создаём роутер
 router = Router()
