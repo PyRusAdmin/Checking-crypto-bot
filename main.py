ed519fc84a3ae470b6258fd2872127035599a986
@PyAdminRU <zh.vitaliy92@gmail.com>
2025-09-05 00:25:21 +0300

    Усилено логирование

--- main.py
+++ main.py
@@ -9,10 +9,14 @@ from aiogram.types import Message
 from handler.handler import register_handler
 from keyboards.keyboards import main_keyboard
 from system.system import router, dp, bot
-
+from loguru import logger
 
 @router.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
+    """Отвечает на команду /start"""
+    user_id = message.from_user.id
+    user_username = message.from_user.username
+    logger.info(f"Пользователь {user_id} {user_username} начал работу с ботом")
     await bot.send_message(text="Приветствуем в боте!",
                            chat_id=message.chat.id, reply_markup=main_keyboard())
 
